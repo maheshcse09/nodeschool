c98dd8a7df9385207399407f00e56c7fea1d1f0a
Anurag Kumar <anuragkr.bk@gmail.com>
2016-11-05 20:16:41 +0530

    Complete javascripting solutions

---
+++ javascripting/14-looping-through-arrays.js
@@ -0,0 +1,11 @@
+var pets = ['cat', 'dog', 'rat'];
+
+for (var i = 0; i < pets.length; i++) {
+    pets[i] += 's';
+}
+// pets.forEach(function(pet, index) {
+//     // console.log(pet);
+//     pets[index] = pet + 's';
+// });
+
+console.log(pets);
